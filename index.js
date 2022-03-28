82eeed73717d9bf16c2c78cdd3594df3b1c279d6
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-28 18:59:25 +0200

    first back test cookies- hEADERS

--- index.js
+++ index.js
@@ -28,7 +28,33 @@ app.use(express.static("public"));
 
 app.use(express.json());
 
+app.use(function (req, res, next) {
 
+  
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials: true");
+res.header("Access-Control-Allow-Methods: GET, POST");
+res.header("Access-Control-Allow-Headers: Content-Type, *");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(cors({
   //origin: "http://localhost:3002",

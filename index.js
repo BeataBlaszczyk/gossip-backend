ebfc15a54a9777c159f3632b4c3f5653dee0f746
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-28 21:27:59 +0200

    first back test cookies- hEADERS-change links

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       sameSite: "none",
-      domain: "https://gossip-frontend.vercel.app/",
+      domain: "gossip-frontend.vercel.app",
       secure: true, // true dla https,
       maxAge: 24*60*60*1000 //one day
     }

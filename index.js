79a9e044f50aa247d7548936671bd4c75881f784
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-29 07:37:35 +0200

    first back test cookies- hEADERS-change links

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       sameSite: "none",
-      //domain: "vercel.com",
+      domain: ".vercel.com",
       secure: true, // true dla https,
       maxAge: 24*60*60*1000 //one day
     }

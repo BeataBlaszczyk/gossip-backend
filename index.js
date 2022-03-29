9bd4279fe0728e46ef1aac7e0f996ec207468e71
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-29 10:23:41 +0200

    still try subdomain cookies

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(function (req, res, next) {
 
   
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://gossip-frontend.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://gossip-frontend.vercel.app');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');

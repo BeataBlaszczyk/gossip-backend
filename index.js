398e64b454099992e8c2ac29e81d6129f34c66e1
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-29 17:40:09 +0200

    add cors expose headers

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.use(session
       path: "/login",
       httpOnly: false,
       hostOnly: false,
-      domain: "gossip-frontend.vercel.app",
+      domain: ".vercel.app",
       //secureProxy: true,
       secure: true, // true dla https,
       maxAge: 24*60*60*1000, //one day,

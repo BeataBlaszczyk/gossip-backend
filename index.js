e2b8b23cdbccf95f2ad355dde24411bc0ea92b85
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-29 09:54:39 +0200

    still try subdomain cookies

--- index.js
+++ index.js
@@ -310,8 +310,8 @@ console.log(user)
 req.login(user, function(err) {
   console.log("is auth => " + req.isAuthenticated())
   if (err) { return next(err); }
- res.cookie("username", "JohnDoe", {domain: ".gossip-frontend.vercel.app", sameSite: "none", secure: true}); 
-   return res.send("DONE3");
+ res.cookie("username", "JohnDoe", {domain: "vercel.app", sameSite: "none", secure: true}); 
+   return res.send("DONE4");
   //return res.redirect("/secrets");
 });
 

3f5339122d1af39b09230178671dd7a9fce0dc71
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-03-29 15:45:12 +0200

    add cors expose headers

--- index.js
+++ index.js
@@ -339,7 +339,7 @@ req.login(user, function(err) {
  res.cookie("username", "JohnDoe", {hostOnly: false, domain:"" , path: "/", sameSite: "none", secure: true}); 
  res.cookie("username", "JohnDoe", {hostOnly: false, path: "/", sameSite: "none", secure: true}); 
  
- return res.send(ress.cookie.name)
+ return res.send('done22')
 });
 
 })

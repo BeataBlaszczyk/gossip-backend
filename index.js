c9f6775382cf64ae8464bc2d03776e9fb4ba92d9
Blaszczyk <beata.blaszczyk@capgemini.com>
2022-04-02 16:02:05 +0200

    google dziala, nie dziala login

--- index.js
+++ index.js
@@ -112,7 +112,7 @@ app.use(session
      //domain:"gossip-frontend.vercel.app",
       //secureProxy: true,
       secure: true, // true dla https,
-      maxAge: 1000 //24*60*60*1000, //one day,
+      maxAge: 24*60*60*1000, //one day,
      
     }
   })
@@ -422,7 +422,7 @@ app.get("/logout", function (req, res) {
  // req.logout();
   req.session.destroy();
   req.session = null;
-  //res.clearCookie("connect.sid")
+  res.clearCookie("connect.sid")
 
  
   //res.redirect('/');
